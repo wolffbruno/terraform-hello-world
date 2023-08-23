c6bb6bed406b206e8906513809478ab9dd7a41c8
BrunoWolff <wolffbruno72@gmail.com>
2023-08-23 14:46:43 -0300

    Changes.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,7 +19,4 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "fiap-iac"
 }
